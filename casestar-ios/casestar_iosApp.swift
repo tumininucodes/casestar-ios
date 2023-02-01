791be4c41c57b20d07702cd9007bd82fbb4ce351
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2023-02-01 07:36:52 +0100

    Initial Commit

---
+++ casestar-ios/casestar_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  casestar_iosApp.swift
+//  casestar-ios
+//
+//  Created by Oluwatumininu Ojo on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct casestar_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
